b80312b4d7693107366b51db4fb7103cb6788959
Prajjwal Jaiswal <73015324+Prajjwal-Jaiswal@users.noreply.github.com>
2024-07-10 22:18:46 +0530

    Update Ticket.java

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Getter; 
 import lombok.NoArgsConstructor;
 import lombok.Setter;
  
